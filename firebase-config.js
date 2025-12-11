d986d5d286986342b05ae7d797cdfde5702a5c17
Alphushigi <alphamiliar@gmail.com>
2025-12-10 20:43:30 -0500

    Update firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCYHdjKiPsT7lbtUv3R-EkeHs_uWnT73Kw",
+  authDomain: "daggerheart-sheets-3d4b0.firebaseapp.com",
+  projectId: "daggerheart-sheets-3d4b0",
+  storageBucket: "daggerheart-sheets-3d4b0.firebasestorage.app",
+  messagingSenderId: "334267596292",
+  appId: "1:334267596292:web:72d95da8740c7352ddff51"
+};
